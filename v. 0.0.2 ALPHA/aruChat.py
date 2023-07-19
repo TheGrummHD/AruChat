828e770e44c36db712d822909dd83cfacaac92ce
Grumm HD <chikibam888@gmail.com>
2023-07-19 20:57:08 +0900

    First Version

---
+++ v. 0.0.2 ALPHA/aruChat.py
@@ -0,0 +1,139 @@
+from colorama import init, Fore, Back, Style
+
+init()
+
+import time
+import random
+import os
+import json
+
+# os.chdir(r'%AppData%')
+# os.getenv('APPDATA')
+
+
+def getCfg():
+	with open('configArusen.json', 'r', encoding = 'utf-8') as f:
+		file = json.load(f)
+
+	return file
+
+def saveCfg(data):
+	with open('configArusen.json', 'w', encoding = 'utf-8') as f:
+		json.dump(data, f, ensure_ascii = False, indent = 4)
+
+# Setup Configurations
+try:
+	cfg = getCfg()
+
+	if cfg['SETTINGS'] == {}:
+		pass
+except:
+	with open('configArusen.json', 'w') as f: f.write(r"{}")
+	
+	cfg = getCfg()
+
+	cfg['SETTINGS'] = {}
+	cfg['SETTINGS']['USERNAME'] = "人間"
+	cfg['SETTINGS']['HELLOLIST'] = ['ハローー！', 'こんにちは。', 'ハイ！', 'こんにちは', 'こんばんわ']
+	cfg['SETTINGS']['CHANGENAME'] = ['名前', '名前を変える']
+
+	saveCfg(cfg)
+
+
+# NEW
+
+# cfg = getCfg()
+
+# cfg['SETTINGS'] = {}
+# cfg['SETTINGS']['USERNAME'] = "人間"
+# cfg['SETTINGS']['HELLOLIST'] = ['ハローー！', 'こんにちは。', 'ハイ！', 'こんにちは', 'こんばんわ']
+# cfg['SETTINGS']['CHANGENAME'] = ['名前', '名前を変える']
+
+# saveCfg(cfg)
+
+
+# Load Configs
+cfg = getCfg()
+
+UserName = cfg['SETTINGS']['USERNAME']
+helloList = cfg['SETTINGS']['HELLOLIST']
+changenameList = cfg['SETTINGS']['CHANGENAME']
+
+saveCfg(cfg)
+
+
+# cmds list
+cmdListM = (Fore.YELLOW + Style.BRIGHT + '・ ランダム\n・ 名前を変える\n・ 出る\n・ ファイル作成\n・ ファイルｓうｔんｇｂｙｒ' + Style.RESET_ALL) #\n・ \n・ \n・ \n・ \n・ \n・ 
+
+
+print(Fore.GREEN + Style.BRIGHT + 'こんにちは、アルチャットです。 \nコマンドリストは' + Fore.YELLOW + ' "help" ' + Fore.GREEN + 'で見れます' + Style.RESET_ALL)
+
+while True:
+	def aiMessage(user_message):
+		message = ''
+		if user_message in helloList:
+			message = random.choice(helloList)
+
+		elif user_message in changenameList:
+			newUserName = str(input(Fore.YELLOW + '新しい名前を入力してください: ' + Style.RESET_ALL))
+
+			data = getCfg()
+
+			data['SETTINGS']['USERNAME'] = newUserName
+
+			saveCfg(data)
+
+			message = (Fore.YELLOW + f'\n* 新しい名前が 「{newUserName}」 になりました。' + Style.RESET_ALL)
+
+		elif user_message == 'ランダム':
+			a = str(input('A: '))
+			b = str(input('B: '))
+
+			message = ('答え: ' + Fore.YELLOW + random.choice([a, b]) + Style.RESET_ALL)
+
+		elif user_message == 'ファイル作成':
+			fileName = str(input(Style.BRIGHT + Fore.YELLOW + "ファイル名: " + Style.RESET_ALL))
+			
+			fileNameFull = '_.txt'
+
+			DOTINNAME = False
+
+			for i in fileName:
+				if i == '.':
+					DOTINNAME = True
+					break
+
+				else:
+					pass
+
+			if DOTINNAME == False:
+				fileType = str(input(Style.BRIGHT + Fore.GREEN + 'ファイルタイプを入力してください。\nファイルタイプリスト: \n' + Style.BRIGHT + Fore.YELLOW + '・　txt' + Style.RESET_ALL + Fore.GREEN + '\n・　json\n・　html\n・　css\nファイルタイプ: ' + Style.RESET_ALL))
+				fileNameFull = f'{fileName}.{fileType}'
+
+			else:
+				fileNameFull = fileName
+			f = open(f'{fileNameFull}', 'w')
+
+		elif user_message == '開ける':
+			filename = input()
+			os.system("start "+filename)
+
+		elif user_message == '出る':
+			quit()
+
+		elif user_message.lower() == 'help':
+			message = (f'\n{cmdListM}\n')
+
+		else:
+			message = '?\n'
+
+		return message
+
+	cfg = getCfg()
+
+	UserName = cfg['SETTINGS']['USERNAME']
+	uMessage = input(f"{UserName}> ")
+
+	aiMessage = aiMessage(str(uMessage))
+	print(aiMessage)
+	# aiMessage = ''
